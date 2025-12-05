9c16dd5a1a8384b1647be46e4398aa8eed0333af
Bhagavan8 <bhagavancv@gmail.com>
2025-12-05 12:38:27 +0530

    CSS Changess

--- js/job-details.js
+++ js/job-details.js
@@ -1485,11 +1485,6 @@ class JobDetailsManager {
                 const regex = new RegExp(`\\b${keyword}\\b`, 'gi');
                 processedText = processedText.replace(regex, '<strong>$&</strong>');
             });
-            processedText = processedText
-                .replace(/\s*,\s*/g, ', ')
-                .replace(/\s*\/\s*/g, ' / ')
-                .replace(/\s*:\s*/g, ': ')
-                .replace(/\s{2,}/g, ' ');
             return processedText;
         };
 
